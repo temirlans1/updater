d477de7fa6e18d8ab85667007fec7d477dc0f226
temirlans1 <temirlan.smagulov@nu.edu.kz>
2019-11-10 23:30:56 +0600

    new update

--- upd.sh
+++ upd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 mkdir imageDataSet
-git clone https://github.com/Margulan707/koramain.git /home/pi/koraupdate/koramain
+git clone https://github.com/temirlans1/koratesting.git /home/pi/koraupdate/koramain
 ret_code=$?
 if [ $ret_code==0 ] 
 then
 	mv /home/pi/koraupdate/kora /home/pi/koraupdate/kora$(date +%Y%m%d%H%M%S)
-	mv /home/pi/koraupdate/koramain /home/pi/koraupdate/kora
+	mv /home/pi/koraupdate/koratesting /home/pi/koraupdate/kora
 fi
 
 echo $ret_code
